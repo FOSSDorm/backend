9d206aff6afe9b91b55cdf09848fbe0ffb06b1c0
Sarvottam Kumar <sarru1291@gmail.com>
2020-10-13 23:09:12 +0530

    initial setup

---
+++ README.md
@@ -0,0 +1,7 @@
+<p align="center"><img src="./assets/images/logo.png" alt="FOSSDorm" width="250" height="250"></p>
+<h1 align="center">FOSSDorm</h1>
+
+<div align="center">
+  <strong>Build your open source profile</strong><br>
+  A <a href="https://www.gnome.org/challenge/">GNOME Challenge 2020 </a> project that aims to bring, engage, and retain new contributors to the open source community.
+</div>
--- index.js
+++ index.js
@@ -1,11 +1,40 @@
+/**
+ * import libraries/packages/functions 
+ */
 const express = require('express')
+const bodyParser=require('body-parser')
+const cors=require('cors')
+require('dotenv').config()
+/**
+ * Initialise constants
+ */
 const app = express();
-const port = 8000;
+const PORT = process.env.PORT;
+const HOST = process.env.HOSTNAME;
 
+/**
+ * Middleware
+ *
+*/
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+/**
+ * Define API at root path
+ * @param {string} path - relative path.
+ * @method {string} author - The author of the book.
+ */
 app.get('/', (req, res) => {
   res.send('FOSSDorm')
 });
 
-app.listen(port, () => {
-  console.log(`${port}!`)
+/**
+ * Listen server request at port number
+ * @param {number} PORT - port number
+ * @callback
+ * @returns {void}
+ */
+app.listen(PORT, () => {
+  console.log(`Server running on http://${HOST}:${PORT}`)
 });
