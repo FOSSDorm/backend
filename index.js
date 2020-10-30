ef0b8c691f3bdbc383609184a8887d50f2ba0bfb
Sarvottam Kumar <sarru1291@gmail.com>
2020-10-30 22:23:56 +0530

    user model and login api

--- index.js
+++ index.js
@@ -1,31 +1,40 @@
-const express = require('express')
-const bodyParser=require('body-parser')
-const cors=require('cors')
-const users=require('./routers/users/users');
-require('dotenv').config()
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const users = require("./routers/users/users");
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
 /**
  * Initialise constants
  */
 const app = express();
-const PORT = process.env.PORT || 8086 ;
+const PORT = process.env.PORT || 8086;
 const HOST = process.env.HOSTNAME || "127.0.0.1";
 
 /**
  * Middleware
  *
-*/
-app.use(cors());
+ */
+app.use(cors({ origin: "http://localhost:3000" }));
+app.use(
+  cookieParser({
+    httpOnly: false,
+    secure: false,
+    signed: true,
+    secret: process.env.SECRET_KEYWORD,
+  })
+);
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
-app.use('/users',users);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/users", users);
 
 /**
  * Define API at root path
  * @param {string} path - relative path.
  * @method {string} author - The author of the book.
  */
-app.get('/', (req, res) => {
-  res.send('Welcome to FOSSDorm!!')
+app.get("/", (req, res) => {
+  res.send("Welcome to FOSSDorm!!");
 });
 
 /**
@@ -35,5 +44,5 @@ app.get('/', (req, res) => {
  * @returns {void}
  */
 app.listen(PORT, () => {
-  console.log(`Server running on http://${HOST}:${PORT}`)
+  console.log(`Server running on http://${HOST}:${PORT}`);
 });
--- routers/users/users.js
+++ routers/users/users.js
@@ -3,18 +3,21 @@ const router = express.Router();
 const axios = require("axios");
 require("dotenv").config();
 const { User } = require("../../db/models/user.model");
+const jwt = require("jsonwebtoken");
 
-router.get("/", (req, res) => {
-  res.send("you have tried get methods");
-});
+// router.get("/", (req, res) => {
+//   res.send("you have tried get methods");
+// });
 /**
- * url for handling GitHub OAuth callback
+ * url for handling GitHub OAuth callback and registering user
  */
 
 router.get("/oauth-callback", (req, res) => {
   const client_id = process.env.GITHUB_CLIENT_ID;
   const client_secret = process.env.GITHUB_CLIENT_SECRET;
   const code = req.query.code;
+
+  // get OAuth access_token
   axios({
     method: "post",
     url: `https://github.com/login/oauth/access_token?client_id=${client_id}&client_secret=${client_secret}&code=${code}`,
@@ -23,10 +26,82 @@ router.get("/oauth-callback", (req, res) => {
     },
   })
     .then((response) => {
-      const accessToken = response.data.access_token;
-      console.log(accessToken);
-      // res.send("You've signed up successfully");
-      res.redirect(`/users/signup?token=${accessToken}`);
+      const access_token = response.data.access_token;
+      console.log(access_token);
+      // get user detail and check if already registered or not
+      axios
+        .get("https://api.github.com/user", {
+          headers: {
+            Authorization: `token ${access_token}`,
+          },
+        })
+        .then((response) => {
+          console.log(response.data);
+          // find user from database
+          User.findOne({ username: response.data.login })
+            .then((user) => {
+              if (user) {
+                // if user already register, just update its token as github generate new token
+                User.updateOne(
+                  { username: response.data.login },
+                  { token: access_token }
+                )
+                  .then((result) => {
+                    console.log(result);
+                    console.log("already signed up");
+                    // generate encrypted token for further authentication
+
+                    let isLoggedIn = "success";
+
+                    res.cookie("token", access_token);
+                    res.redirect(
+                      301,
+                      `http://localhost:3000/user/${response.data.login}/${isLoggedIn}`
+                    );
+                  })
+                  .catch((err) => {
+                    console.error(err);
+                    let isLoggedIn = "failure";
+                    res.redirect(
+                      301,
+                      `http://localhost:3000/user/${response.data.login}/${isLoggedIn}`
+                    );
+                  });
+              } else {
+                // if user not register, create account and save
+                let userData = {
+                  username: response.data.login,
+                  token: access_token,
+                };
+                const user = new User(userData);
+                user
+                  .save()
+                  .then((data) => {
+                    console.log(data);
+                    console.log("New user created");
+
+                    let isLoggedIn = "success";
+                    res.cookie("token", access_token);
+                    res.redirect(
+                      301,
+                      `http://localhost:3000/user/${response.data.login}/${isLoggedIn}`
+                    );
+                  })
+                  .catch((error) => {
+                    console.error(error);
+                    let isLoggedIn = "failure";
+                    res.redirect(
+                      301,
+                      `http://localhost:3000/user/${response.data.login}/${isLoggedIn}`
+                    );
+                  });
+              }
+            })
+            .catch((err) => console.err(err));
+        })
+        .catch((error) => {
+          console.error(error);
+        });
     })
     .catch((error) => {
       console.error(error);
@@ -34,32 +109,29 @@ router.get("/oauth-callback", (req, res) => {
 });
 
 /**
- * After oauth from GitHub, register user
+ * Check if user is logged in or not
  */
-router.get("/signup", (req, res) => {
-  const access_token = req.query.token;
-  axios
-    .get("https://api.github.com/user", {
-      headers: {
-        Authorization: `token ${access_token}`,
-      },
-    })
-    .then((res) => {
-      console.log(res.data);
-      let userData = { username: res.data.login, token: access_token };
-      if (!User.find({ username: res.data.username })) {
-        var user = new User(userData);
-        user
-          .save()
-          .then((data) => console.log(data))
-          .catch((error) => console.error(error));
-      } else {
-        console.log("user already signed up");
-      }
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+router.post("/auth", (req, res) => {
+  console.log(req.body);
+  let auth_token = req.body.auth_token;
+  if (auth_token == null || auth_token == " " || auth_token == undefined) {
+    res.send({ isLoggedIn: false });
+  } else {
+    console.log(auth_token);
+    User.findOne({ token: auth_token })
+      .then((user) => {
+        if (user) {
+          res.send({ isLoggedIn: true, username: user.username });
+        } else {
+          res.send({ isLoggedIn: false });
+        }
+      })
+      .catch((err) => console.error(err));
+  }
 });
 
+router.get("/logout", (req, res) => {
+  console.log("logout request");
+  res.clearCookie("token").redirect(`http://localhost:3000`);
+});
 module.exports = router;
